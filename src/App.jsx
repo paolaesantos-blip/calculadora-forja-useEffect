821d2825e10f4a96b4cbc6d8b693c90602d1f87a
paolaesantos-blip <paola.e.santos@aluno.senai.br>
2025-11-11 16:57:38 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [n1, setN1] = useState()
+  const [n2, setN2] = useState()
+  const [respSoma, setRespSoma] = useState()
+  const [respSub, setRespSub] = useState()
+  const [respMult, setRespMulti] = useState()
+  const [respDiv, setRespDiv] = useState()
+
+
+    useEffect(() => {
+      if (n1 !== undefined && n2 !== undefined && !isNaN(n1) && !isNaN(n2)) {
+        setRespSub(parseFloat(n1) - parseFloat(n2))
+        setRespSoma(parseFloat(n1) + parseFloat(n2))
+        setRespMulti(parseFloat(n1) * parseFloat(n2))
+        setRespDiv(parseFloat(n1) / parseFloat(n2))
+
+
+
+      } else {
+
+
+      }
+    }), [n1, n2]
+
 
   return (
+
     <>
+      <h1 className={styles.title}>calculadora</h1>
+      <div className={styles.wrap}>
+        <input type="number" onChange={(e) => setN1(e.target.value)} value={n1} placeholder="insira um numero" />
+        <input type="number" onChange={(e) => setN2(e.target.value)} value={n2} placeholder="insira outro número" />
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Resultado da soma</h2>
+        <h3>{respSoma}</h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <h2>Resultado da sub</h2>
+        <h3>{respSub}</h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <h2>Resultado da mult</h2>
+        <h3>{respMult}</h3>
+      </div>
+
+      <div>
+        <h2>Resultado da div</h2>
+        <h3>{respDiv}</h3>
+      </div>
+
+
+
     </>
   )
 }
